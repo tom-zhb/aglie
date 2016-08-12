d02574111696a011e6712143edad9dcf24bbbaf0
tom-zhb <846029405@qq.com>
2016-08-12 12:20:20 +0800

    test

---
+++ src/main/java/com/alige/config/Const.java
@@ -0,0 +1,11 @@
+package com.alige.config;
+
+/**
+ * Global constants definition
+ */
+public final class Const {
+    
+	private Const() {}
+	
+	public static final String DEFAULT_ENCODING = "UTF-8";
+}
